340c3b3e038be832a411d0573d1fecaf0a7cfd3b
flylikeahokie <nttalp@gmail.com>
2025-05-20 09:51:40 -0500

    added fps control and player class to draw player sprite on screen

--- main.py
+++ main.py
@@ -3,19 +3,29 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    #this block of code was required for an early step in the project, but doesn't seen to be needed moving forward - leaving for the time being
+    """print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}")"""
 
-    while screen.fill("black"):
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
